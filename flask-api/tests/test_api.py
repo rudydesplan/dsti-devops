6ee4348d0a8c5f6c1290c364c4d08a75587a88c5
Mohamed Hamiche <mohamed.hamiche@mydatamodels.com>
2023-04-08 13:47:44 +0200

    test: first api test

---
+++ flask-api/tests/test_api.py
@@ -0,0 +1,26 @@
+import requests
+
+
+def test_get_endpoint_returns_json_output():
+    # Define the endpoint URL
+    endpoint_url = "http://localhost/avocados/0"
+
+    # Make a GET request to the endpoint
+    response = requests.get(endpoint_url)
+
+    # Check that the response status code is 200 OK
+    assert response.status_code == 200
+
+    # Check that the response content type is JSON
+    assert response.headers["Content-Type"] == "application/json"
+
+    # Check that the response body is a valid JSON object
+    try:
+        json_data = response.json()
+    except ValueError:
+        assert False, "Response body is not valid JSON"
+
+    # Check that the JSON object has the expected keys
+    expected_keys = ["date", "small_plu", "state"]
+    assert set(expected_keys).issubset(json_data.keys()), \
+        f"Expected keys {expected_keys} not found in response JSON"
