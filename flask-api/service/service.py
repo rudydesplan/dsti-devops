e584dc89e1b30d2f031c8e7256b973e37a11a487
rudy desplan <desrudy@hotmail.com>
2023-04-19 22:25:30 +0200

    Update service.py

--- flask-api/service/service.py
+++ flask-api/service/service.py
@@ -7,16 +7,32 @@ from flask import Flask, abort, jsonify, make_response, render_template
 from modules.preparation import AvocadoPrep
 from modules.preparation.conf import DATA_LOCATION
 
+class MongoConnector:
+    def __init__(self, uri, db_name):
+        self.client = MongoClient(uri)
+        self.db = self.client[db_name]
+
+     # Créer un index unique pour éviter les doublons
+    def create_unique_index(self, collection_name):
+        try:
+            self.db[collection_name].create_index("unique_id", unique=True)
+        except pymongo.errors.OperationFailure as e:
+            print(f"Erreur lors de la création de l'index unique : {e}")
+
+    def upsert_data(self, collection_name, data):
+        collection = self.db[collection_name]
+        for item in data:
+            unique_id = create_unique_id(item)
+            item["unique_id"] = unique_id
+            collection.update_one({"unique_id": unique_id}, {"$set": item}, upsert=True)
+
 # Remplacez <username> et <password> par vos informations d'identification
 MONGODB_URI = "mongodb+srv://<dsti-devops>:<dsti-devops>@cluster0.piza0cu.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(MONGODB_URI)
-db = client["avocado_db"]
 
 app = Flask(__name__)
 
-def create_unique_id(item):
-    unique_id = f'{item["date"]}-{item["small_plu"]}-{item["state"]}-{item["average_size_bags"]}-{item["region"]}-{item["season"]}'
-    return unique_id
+mongo_connector = MongoConnector(MONGODB_URI, "avocado_db")
+mongo_connector.create_unique_index("avocados")
 
 @app.route("/")
 def home():
@@ -28,15 +44,8 @@ def get_avocados():
     preparation = AvocadoPrep(dataset_location=DATA_LOCATION)
     prepared_json = preparation.prepare(Json=True)
 
-    # Créer un index unique pour éviter les doublons
-    avocados_collection = mongo.db.avocados
-    create_unique_index(avocados_collection)
-
-    # Mettre à jour ou insérer les données JSON dans MongoDB
-    for item in prepared_json:
-        unique_id = create_unique_id(item)
-        item["unique_id"] = unique_id
-        avocados_collection.update_one({"unique_id": unique_id}, {"$set": item}, upsert=True)
+    # Insérer ou mettre à jour les données JSON dans MongoDB
+    mongo_connector.upsert_data("avocados", prepared_json)
 
     response = jsonify(prepared_json)
     return response
