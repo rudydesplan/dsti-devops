e74637b982195d1aa23b80e885c469910a17dee7
rudy desplan <desrudy@hotmail.com>
2023-04-19 22:15:26 +0200

    Update service.py

--- flask-api/service/service.py
+++ flask-api/service/service.py
@@ -1,11 +1,22 @@
+import pymongo
+from pymongo import MongoClient
+
 import pandas as pd
 from flask import Flask, abort, jsonify, make_response, render_template
 
 from modules.preparation import AvocadoPrep
 from modules.preparation.conf import DATA_LOCATION
 
+# Remplacez <username> et <password> par vos informations d'identification
+MONGODB_URI = "mongodb+srv://<dsti-devops>:<dsti-devops>@cluster0.piza0cu.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(MONGODB_URI)
+db = client["avocado_db"]
+
 app = Flask(__name__)
 
+def create_unique_id(item):
+    unique_id = f'{item["date"]}-{item["small_plu"]}-{item["state"]}-{item["average_size_bags"]}-{item["region"]}-{item["season"]}'
+    return unique_id
 
 @app.route("/")
 def home():
@@ -16,9 +27,19 @@ def home():
 def get_avocados():
     preparation = AvocadoPrep(dataset_location=DATA_LOCATION)
     prepared_json = preparation.prepare(Json=True)
+
+    # Créer un index unique pour éviter les doublons
+    avocados_collection = mongo.db.avocados
+    create_unique_index(avocados_collection)
+
+    # Mettre à jour ou insérer les données JSON dans MongoDB
+    for item in prepared_json:
+        unique_id = create_unique_id(item)
+        item["unique_id"] = unique_id
+        avocados_collection.update_one({"unique_id": unique_id}, {"$set": item}, upsert=True)
+
     response = jsonify(prepared_json)
     return response
-
 
 @app.route("/avocados/<index>")
 def get_by_id(index):
