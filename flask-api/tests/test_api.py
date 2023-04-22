2f577830586227c8e95457d68e2a37ca50593217
rudy desplan <desrudy@hotmail.com>
2023-04-22 07:30:48 +0200

    Update test_api.py

--- flask-api/tests/test_api.py
+++ flask-api/tests/test_api.py
@@ -166,12 +166,31 @@ def test_delete_endpoint_deletes_existing_avocado_document():
     # Define the endpoint URL
     unique_id = 2
     endpoint_url = f"http://localhost:5000/avocados/{unique_id}"
+    
+    # Define the avocado document data
+    avocado_data = {
+        "average_size_bags": 5,
+        "date": "2023-01-01",
+        "region": "West",
+        "season": "spring",
+        "small_plu": 12345.6,
+        "state": "California",
+    }
+    
+    # Make a POST request to the endpoint
+    response = requests.post(endpoint_url, json=avocado_data)
 
     # Make a DELETE request to the endpoint
     response = requests.delete(endpoint_url)
 
     # Check that the response status code is 204 No Content
     assert response.status_code == 204
+
+    # Test GET endpoint to confirm that the avocado document has been deleted
+    response = requests.get(endpoint_url)
+
+    # Check that the response status code is 404 Not Found
+    assert response.status_code == 404
 
 #4b Test DELETE endpoint with a non-existent document
 def test_delete_endpoint_with_non_existent_document_returns_404():
