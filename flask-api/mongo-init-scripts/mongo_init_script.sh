b46cad6a570d40c9a2e04abdfd63e8600c52f8f1
Mohamed Hamiche <mohamed.hamiche@mydatamodels.com>
2023-05-05 22:30:16 +0200

    fix: docker-compose

---
+++ flask-api/mongo-init-scripts/mongo_init_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# create a user with read and write access to the avocado_db database
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+    db.createUser({
+        user: "$MONGO_INITDB_USERNAME",
+        pwd: "$MONGO_INITDB_PASSWORD",
+        roles: [{
+            role: "readWrite",
+            db: "$MONGO_INITDB_DATABASE"
+        }]
+    })
+EOF
